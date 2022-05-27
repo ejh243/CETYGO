b49fd9bda08d7a7455c52b0eb69697bb7364a97a
ejh243 <e.j.hannon@exeter.ac.uk>
2022-05-27 17:08:12 +0100

    document datasets

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Whole blood DNA methylation profiles.
+#'
+#' A sample dataset of whole blood DNA methylation profiles from 10 individuals
+#'
+#' @format A matrix with 599 rows and 10 variables
+
+#' Coefficients for 6 blood cell types
+#'
+#' Coefficinets for blood cell types for use in deconvoluting cell compoition from whole blood profiles. These data were generated using the pickCompProbes() function from the minfi package and DNA methylation data from the FlowSorted.Blood.450k R package. 
+#'
+#' @format A matrix with 600 rows and 6 variables
+
